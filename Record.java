f005053da0686792dfe23da83fe26dee4b72b359
dwighty88 <shirleymarcaida@vicky-os-macbook-pro.local>
2014-02-16 00:35:08 +0800

    Record class: Constructor, Getters and Setters
    Added methods for Getters and Setters and the Constructor for the
    Record class. No code inside the methods yet :))

--- Record.java
+++ Record.java
@@ -1,4 +1,60 @@
 public class Record
 {
-	
+	public Record(Patient patient, String referenceNumber, String specimen, String physician, String pathologist, Date dateReceived, int recordType)
+	{
+
+	}
+
+	//Getters
+	public String getPatient()
+	{}
+
+	public String getReferenceNumber()
+	{}
+
+	public String getSpecimen()
+	{}
+
+	public String getPhysician()
+	{}
+
+	public String getPathologist()
+	{}
+
+	public String getBiopsy()
+	{}
+
+	public Date getDateReceived()
+	{}
+
+	public Date getDateCompleted()
+	{}
+
+	public String getRemarks()
+	{}
+
+	public Date getLastDateModified()
+	{}
+
+	//Setters
+	public void setSpecimen(String specimen)
+	{}
+
+	public void setPhysician(String physician)
+	{}
+
+	public void setPathologist(String pathologist)
+	{}
+
+	public void setBiopsyFindings(String biopsyFindings)
+	{}
+
+	public void setDateCompleted(Date dateCompleted)
+	{}
+
+	public void setRenarks(String remarks)
+	{}
+
+	public void setLastDateModified(Date lastDateModified)
+	{}
 }
