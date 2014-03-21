79e799a2d0b03491059507c5451a7852f48ca290
jraquiinoo <jraquiinoo@gmail.com>
2014-03-21 11:18:16 +0800

    date fix

--- Introse 3-19-2014/src/org/introse/gui/combobox/DatePicker.java
+++ Introse 3-19-2014/src/org/introse/gui/combobox/DatePicker.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.Calendar;
 
 import javax.swing.JComboBox;
@@ -12,7 +14,7 @@ import javax.swing.JPanel;
 
 import org.introse.core.CustomCalendar;
 
-public class DatePicker extends JPanel {
+public class DatePicker extends JPanel implements ItemListener{
 
 	private static String[]  monthNames = {"January", "February", 
 		 "March", "April", "May", "June", "July", "August", 
@@ -40,6 +42,8 @@ public class DatePicker extends JPanel {
 		day= new JComboBox<String>(monthDays);
 		month= new JComboBox<String>(monthNames);
 		year= new JComboBox<String>(years);
+		month.addItemListener(this);
+		year.addItemListener(this);
 		day.setBorder(null);
 		month.setBorder(null);
 		year.setBorder(null);
@@ -102,5 +106,117 @@ public class DatePicker extends JPanel {
 		this.day.setEnabled(isEnabled);
 		this.month.setEnabled(isEnabled);
 		this.year.setEnabled(isEnabled);
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) 
+	{
+		Object source = e.getSource();
+		int change = e.getStateChange();
+		
+		if(change == ItemEvent.SELECTED)
+		{
+			if(source == month)
+			{
+				int selectedMonth = month.getSelectedIndex();
+				
+				if(selectedMonth == 0 || selectedMonth == 2 || selectedMonth == 4 || 
+						selectedMonth == 6 || selectedMonth == 7 || selectedMonth == 9 ||
+						selectedMonth == 0 || selectedMonth == 11)
+				{
+					int i = day.getItemCount() + 1;
+					String d = "" + i;
+					while(day.getItemCount() < 31)
+					{
+						day.addItem(d);
+						i++;
+						d = "" + i;
+					}
+				}
+				else if(selectedMonth == 3 || selectedMonth == 5 || 
+						selectedMonth == 8 || selectedMonth == 10)
+				{
+					if(day.getItemCount() == 31)
+						day.removeItemAt(30);
+					else
+					{
+						int i = day.getItemCount() + 1;
+						String d = "" + i;
+						while(day.getItemCount() < 30)
+						{
+							day.addItem(d);
+							i++;
+							d = "" + i;
+						}
+					}
+				}
+				else if(selectedMonth == 1)
+				{
+					int selectedYear = Integer.parseInt((String)year.getSelectedItem());
+					if(selectedYear % 4 == 0 || (selectedYear % 100 == 0 && selectedYear % 400 == 0))
+					{
+						if(day.getItemCount() == 31)
+							day.removeItemAt(30);
+						if(day.getItemCount() == 30)
+							day.removeItemAt(29);
+						if(day.getItemCount() == 29)
+							return;
+						
+						int i = day.getItemCount() + 1;
+						String d = "" + i;
+						while(day.getItemCount() < 29)
+						{
+							day.addItem(d);
+							i++;
+							d = "" + i;
+						}
+					}
+					else
+					{
+						if(day.getItemCount() == 31)
+							day.removeItemAt(30);
+						if(day.getItemCount() == 30)
+							day.removeItemAt(29);
+						if(day.getItemCount() == 29)
+							day.removeItemAt(28);
+					}
+				}
+			}
+			else if(source == year)
+			{
+				if(month.getSelectedIndex() == 1)
+				{
+					int selectedYear = Integer.parseInt((String)year.getSelectedItem());
+					if(selectedYear % 4 == 0 || (selectedYear % 100 == 0 && selectedYear % 400 == 0))
+					{
+						if(day.getItemCount() == 31)
+							day.removeItemAt(30);
+						if(day.getItemCount() == 30)
+							day.removeItemAt(29);
+						if(day.getItemCount() == 29)
+							return;
+						
+						int i = day.getItemCount() + 1;
+						String d = "" + i;
+						while(day.getItemCount() < 29)
+						{
+							day.addItem(d);
+							i++;
+							d = "" + i;
+						}
+					}
+					else
+					{
+						if(day.getItemCount() == 31)
+							day.removeItemAt(30);
+						if(day.getItemCount() == 30)
+							day.removeItemAt(29);
+						if(day.getItemCount() == 29)
+							day.removeItemAt(28);
+					}
+				}
+			}
+		}
+		
 	}
 }
