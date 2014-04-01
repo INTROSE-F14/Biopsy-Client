b2095538802376836f779012bc28c4bc14f45561
davidnicholai <nicholai.david@gmail.com>
2014-04-01 15:23:46 +0800

    Added Export UI

--- Introse 3-19-2014/src/org/introse/gui/panel/BackupAndRestorePanel.java
+++ Introse 3-19-2014/src/org/introse/gui/panel/BackupAndRestorePanel.java
@@ -7,6 +7,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,9 +19,9 @@ import org.introse.gui.window.MainMenu;
 
 public class BackupAndRestorePanel extends JPanel {
 	
-	private JLabel lblBackup, lblRestore;
-	private JTextField backupDirectory, restoreDirectory;
-	private JButton btnBackup, btnRestore, btnBackupDirectory, btnRestoreDirectory;
+	private JLabel lblBackup, lblRestore, lblExport;
+	private JTextField backupDirectory, restoreDirectory, exportDirectory;
+	private JButton btnBackup, btnRestore, btnExport, btnBackupDirectory, btnRestoreDirectory, btnExportDirectory;
 	
 	public BackupAndRestorePanel() 
 	{
@@ -75,33 +76,80 @@ public class BackupAndRestorePanel extends JPanel {
 		c.gridwidth = 2;
 		c.insets = new Insets(0,0,20,0);
 		add(btnRestore, c);
+
+		c.anchor = GridBagConstraints.LINE_START;
+		c.gridwidth = 3;
+		c.gridx = 0;
+		c.gridy = y++;
+		c.insets = new Insets(0,0,0,0);
+		add(lblExport, c);
+		c.gridwidth = 2;
+		c.gridy = y;
+		c.insets = new Insets(0,0,10,5);
+		add(exportDirectory, c);
+		c.gridwidth = 1;
+		c.gridx= 2;
+		c.gridy = y++;
+		c.insets = new Insets(0,0,10,0);
+		add(btnExportDirectory, c);
+		c.anchor = GridBagConstraints.LINE_END;
+		c.gridy = y;
+		c.gridx = 1;
+		c.gridwidth = 2;
+		c.insets = new Insets(0,0,20,0);
+		add(btnExport, c);
 	}
 	
 	private void initializeComponents()
 	{
+		
+
+		
+		
+		
+		
+		
 		lblBackup = new JLabel("Backup");
 		lblRestore = new JLabel("Restore");
+		lblExport = new JLabel("Export");
 		
 		lblBackup.setFont(MainMenu.PRIMARY_FONT.deriveFont(StyleConstants.HEADER));
 		lblRestore.setFont(lblBackup.getFont());
+		lblExport.setFont(lblBackup.getFont());
 		
 		backupDirectory = new JTextField(30);
 		restoreDirectory = new JTextField(30);
+		exportDirectory = new JTextField(30);
 		backupDirectory.setFont(getFont().deriveFont(StyleConstants.MENU));
 		restoreDirectory.setFont(backupDirectory.getFont());
+		exportDirectory.setFont(restoreDirectory.getFont());
 		backupDirectory.setEditable(false);
 		restoreDirectory.setEditable(false);
+		exportDirectory.setEditable(false);		
 		
-		btnBackup = new JButton("create backup");
-		btnRestore = new JButton("restore backup");
+		btnBackup = new JButton("");
+		btnBackup.setBorderPainted(false);
+		btnBackup.setContentAreaFilled(false);
+		btnBackup.setIcon(new ImageIcon(BackupAndRestorePanel.class.getResource("/res/icons/btnBackup.png")));
+		btnBackup.setRolloverIcon(new ImageIcon(BackupAndRestorePanel.class.getResource("/res/icons/btnBackupHover.png")));
+		btnBackup.setEnabled(false);
+		
+		btnRestore = new JButton("");
+		btnRestore.setBorderPainted(false);
+		btnRestore.setContentAreaFilled(false);
+		btnRestore.setIcon(new ImageIcon(BackupAndRestorePanel.class.getResource("/res/icons/btnRestore.png")));
+		btnRestore.setRolloverIcon(new ImageIcon(BackupAndRestorePanel.class.getResource("/res/icons/btnRestoreHover.png")));
+		btnRestore.setEnabled(false);
+		
+		btnExport = new JButton("Export");
+		btnExport.setEnabled(false);
+		
 		btnBackupDirectory = new JButton("...");
 		btnRestoreDirectory = new JButton("...");
-		btnBackup.setFont(getFont().deriveFont(StyleConstants.MENU));
-		btnRestore.setFont(btnBackup.getFont());
-		btnBackupDirectory.setFont(btnBackup.getFont());
-		btnRestoreDirectory.setFont(btnBackupDirectory.getFont());
-		btnBackup.setEnabled(false);
-		btnRestore.setEnabled(false);
+		btnExportDirectory = new JButton("...");
+		btnBackupDirectory.setFont(getFont().deriveFont(StyleConstants.MENU));
+		btnRestoreDirectory.setFont(getFont().deriveFont(StyleConstants.MENU));
+		btnExportDirectory.setFont(getFont().deriveFont(StyleConstants.MENU));
 	}
 	
 	public void addListener(ActionListener listener)
